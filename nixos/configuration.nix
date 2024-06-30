0065346f821f6065978d975457ad50c1c0980771
InDoNax <avilykn@gmail.com>
2024-06-30 17:28:21 +0000

    updated configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -46,6 +46,17 @@
     xwayland.enable = true;
   };
 
+  environment.sessionVariables = { 
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";  
+  };
+
+  hardware = {
+    opengl.enable = true;
+    nvidia.modesetting.enable = true;
+  };
+
+
   nixpkgs.config.allowUnfree = true;
 
   environment.systemPackages = with pkgs; [
@@ -55,12 +66,39 @@
     dolphin
     wofi
     vscode  
-    opera
+    (opera.override {proprietaryCodecs = true;})
     git
     spotify
     terraform 
     waybar
-    hyprpaper 
+    hyprpaper
+    terraform
+    azure-cli
+    hyprcursor
+    hyprlock
+    dunst
+    libnotify
+    rofi-wayland
+    discord
+  ];
+
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  
+  sound.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
+  }; 
+
+  fonts.packages = with pkgs; [
+    font-awesome
+    roboto
+    helvetica-neue-lt-std
   ];
 
 
